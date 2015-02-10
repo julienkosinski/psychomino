9ff14b144f023c155c53214e7983a2576e328ed7
julienkosinski <julien.kosinski@gmail.com>
2015-02-10 19:38:40 +0100

    Last try assets

--- psychomino/settings.py
+++ psychomino/settings.py
@@ -81,11 +81,14 @@ class Common(Configuration):
 
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/1.7/howto/static-files/
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATIC_URL = '/static/'
+    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+    PROJECT_DIR = os.path.join(PROJECT_ROOT,'../psychomino')
 
+    STATIC_URL = '/static/'
+    
+    STATIC_ROOT = os.path.join(PROJECT_ROOT,'staticfiles/')
     STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
+        os.path.join(PROJECT_DIR,'static/'),
     )
 
 class Development(Common):
