8ca38267cf3256e58ca514bf93d47cbd02340a05
julienkosinski <julien.kosinski@gmail.com>
2015-02-10 19:38:40 +0100

    Last try assets

--- psychomino/urls.py
+++ psychomino/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 import djadmin2
 
@@ -11,4 +12,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin2/', include(djadmin2.default.urls)),
+)
+if not settings.DEBUG:
+urlpatterns += patterns('',
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
