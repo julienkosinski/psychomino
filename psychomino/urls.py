23eb259e76f258ed9cde36f66b2b519dafee6422
julienkosinski <julien.kosinski@gmail.com>
2015-02-16 13:40:06 +0100

    Clean url.py

--- psychomino/urls.py
+++ psychomino/urls.py
@@ -12,7 +12,3 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('rest_framework.urls', namespace='rest_framework')),
 )
-if not settings.DEBUG:
-urlpatterns += patterns('',
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-)
