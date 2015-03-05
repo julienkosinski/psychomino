f4ff79f429c389dca87edb0aff40241262afc8c6
julienkosinski <julien.kosinski@gmail.com>
2015-03-05 13:46:16 +0100

    ImageKit transform image. have to link it with form correctly I guess...

--- lessons/models.py
+++ lessons/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill, Adjust
 
 
 class Lesson(models.Model):
@@ -20,15 +22,26 @@ class Branch(models.Model):
 
 
 class Element(models.Model):
-    element_content = models.TextField()
     ELEMENT_TYPE_CHOICE = (
         ('IMG', 'Image'),
         ('TEXT', 'Text'),
     )
     element_type = models.CharField(max_length=4, choices=ELEMENT_TYPE_CHOICE)
+    if element_type == 'IMG':
+        element_content = self.ElementImage()
+    else:
+        element_content = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Date de création")
     modify_date = models.DateTimeField(auto_now_add=False, auto_now=True, verbose_name="Date de modification")
     element_branch = models.ForeignKey('Branch', related_name='elements')
+
+    def ElementImage():
+        element_image = ProcessedImageField(upload_to='elementsImg',
+                                            processors=[ResizeToFill(200, 200), Adjust(color=0.5)],
+                                            format='PNG',
+                                            options={'quality': 100})
+        element = ElementImage.objects.all()[0]
+        return element.element_image.url
 
     def __str__(self):
         return self.element_content
