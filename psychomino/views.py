16bfe0e0368f3a6fcaf433a798df30a52c6d0d44
Rouksana <rouksana.shannon@gmail.com>
2015-04-16 14:48:29 +0200

    fix dep

--- psychomino/views.py
+++ psychomino/views.py
@@ -1,26 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from selenium import webdriver
-from pyvirtualdisplay import Display
-from django.contrib.sites.models import Site
-
-def save_and_download_screenshots(request, pk):
-	display = Display(visible=0, size=(1960, 1120))
-	display.start()
-	browser = webdriver.Firefox()
-	current_site = Site.objects.get_current()
-	
-	browser.get('http://'+current_site.domain+'/api/lessons/'+pk)
-	
-	browser.save_screenshot('/static/images/screenshot'+pk+'.png')
-	browser.quit()
-	display.stop()
 
 def init(request):
     return render(request, 'psychomino/index.html')
 
 def home(request, pk):
-	return render(request, 'psychomino/index.html')
+    return render(request, 'psychomino/index.html')
 
 def rules(request):
     return render(request, 'psychomino/rules.html')
