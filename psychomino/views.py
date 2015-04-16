0616518b6942d9e97f3c927bef9097412b5fc2e9
RomainDeSaJardim <romain.desajardim@gmail.com>
2015-04-16 21:34:45 +0200

    view contact send mail

--- psychomino/views.py
+++ psychomino/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from selenium import webdriver
 from pyvirtualdisplay import Display
 from django.contrib.sites.models import Site
+from psychomino.forms import ContactForm
+from django.core.mail import send_mail
+from django.shortcuts import redirect
 
 def save_and_download_screenshots(request, pk):
 	display = Display(visible=0, size=(1960, 1120))
@@ -26,4 +29,23 @@ def rules(request):
     return render(request, 'psychomino/rules.html')
 
 def contact(request):
-    return render(request, 'psychomino/contact.html')
+    if request.method == 'POST':  # S'il s'agit d'une requête POST
+        form = ContactForm(request.POST)  # Nous reprenons les données
+
+        if form.is_valid(): # Nous vérifions que les données envoyées sont valides
+
+            # Ici nous pouvons traiter les données du formulaire
+            nom = form.cleaned_data['nom']
+            sender = form.cleaned_data['sender']
+            message = form.cleaned_data['message']
+
+            # Nous pourrions ici envoyer l'e-mail grâce aux données que nous venons de récupérer
+
+            send_mail('Psychomino contact. ' + sender , message, sender, ['romain.desajardim@gmail.com'], fail_silently=False)
+
+            return redirect('psychomino.views.home')
+
+    else: # Si ce n'est pas du POST, c'est probablement une requête GET
+        form = ContactForm()  # Nous créons un formulaire vide
+
+    return render(request, 'psychomino/contact.html', locals())
