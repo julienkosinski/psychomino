f98db711c6e9c67f2af0642f364cb3194b51cd78
Rouksana <rouksana.shannon@gmail.com>
2015-04-16 14:40:22 +0200

    share social

--- psychomino/views.py
+++ psychomino/views.py
@@ -20,7 +20,7 @@ def init(request):
     return render(request, 'psychomino/index.html')
 
 def home(request, pk):
-    return render(request, 'psychomino/index.html')
+	return render(request, 'psychomino/index.html')
 
 def rules(request):
     return render(request, 'psychomino/rules.html')
